4f1d12e81c80e1872037a20c68a35ff50688565e
izupanda <izumi.handa@pandalab.jp>
2024-10-03 23:06:09 +0900

    update tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,9 @@ import type { Config } from "tailwindcss";
 const config: Config = {
     darkMode: ["class"],
     content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     // 必要に応じて他のパスを追加
   ],
   theme: {
@@ -73,7 +75,8 @@ const config: Config = {
   	borderRadius: {
   		lg: 'var(--radius)',
   		md: 'calc(var(--radius) - 2px)',
-  		sm: 'calc(var(--radius) - 4px)'
+  		sm: 'calc(var(--radius) - 4px)',
+  		'full': '9999px',
   	},
   	keyframes: {
   		'accordion-down': {
