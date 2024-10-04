e17507bd8f79040aed741b8277c19d146392b18f
izupanda <izumi.handa@pandalab.jp>
2024-10-04 14:30:35 +0900

    add contact and pp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,7 @@
+
+'use client'
+
+
 import Link from "next/link"
 import { ExternalLink } from "lucide-react"
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+
 import * as React from "react"
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
