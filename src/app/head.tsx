cf9478a1906557daa1139efb59bb1c2ce35b1131
izupanda <izumi.handa@pandalab.jp>
2024-10-10 09:47:20 +0900

    push to tailyjp

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function CustomHead() {
+  return (
+    <>
+      <title>Your Page Title</title>
+      <meta name="description" content="Your page description" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
